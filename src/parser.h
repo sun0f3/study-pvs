106497828dce282db3915f9a94703bca92a984f8
Игорь <smartello@smartello-desktop.(none)>
2009-12-06 15:00:51 +0300

    Start to define parser.h interface

--- src/parser.h
+++ src/parser.h
@@ -4,3 +4,19 @@ Project: POP3 Server
 Company: BMSTU
 Description: Parser for protocol
 */
+
+#pragma once
+
+#define BUF_LEN 100
+#define MAX_PATH 256
+#define MAIL_ATTR_SIZE 10
+
+/*!
+ * \todo Вообще говоря нужно параметром передавать
+ */
+#define MAIL_ROOT_PATH "maildrop"
+
+#define POP3_STATE_AUTHORIZATION 1
+#define POP3_STATE_TRANSACTION 2
+#define POP3_STATE_UPDATE 4
+
