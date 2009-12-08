7c8e07f0e400cfb5b89d4aeff36dc94153e8e7b8
Eugene Varlamov <varlamoved@gmail.com>
2009-12-08 17:38:57 +0300

    Add:Logger struct state added

--- src/logger.h
+++ src/logger.h
@@ -7,5 +7,20 @@ Description: Logger.h
 #ifndef _LOGGER_H__
 #define _LOGGER_H__
 
+#define PMQ_NAME "/pop3smq"
+
+struct logger_struct {
+    int fStop;
+    pthread_t thrd;
+    mqd_t mqueue;
+    long maxsz;
+    int logfd;
+    char *message_buf;
+    pthread_mutex_t mutex_log;
+
+} logger;
+
+
+
 
 #endif
