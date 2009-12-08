1e2d67e4b234001de2838eb6d564bd6df09b7bf7
Eugene Varlamov <varlamoved@gmail.com>
2009-12-08 15:11:22 +0300

    Add:main.c main function

--- src/main.c
+++ src/main.c
@@ -4,3 +4,19 @@ Project: POP3 Server
 Company: BMSTU
 Description: main file pop3server
 */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+
+#include "logger.h"
+#include "serv.h"
+
+
+int main(int argc, char** argv)
+{
+
+    return 0;
+}
+
+
