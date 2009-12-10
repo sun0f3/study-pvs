e1f2f889846845292023e7ef55c0205cd0a5a48c
Eugene Varlamov <varlamoved@gmail.com>
2009-12-10 17:48:29 +0300

    Add:Logger function start_logger

--- src/logger.c
+++ src/logger.c
@@ -4,3 +4,14 @@ Project: POP3 Server
 Company: BMSTU
 Description: logger.c
 */
+#include "logger.h"
+
+int start_logger(char * logfname)
+{
+    init_logger(logfname);
+    mqlog("Starting logger...");
+    logger.fStop = 0;
+    return pthread_create(&logger.thrd, NULL, logthread, NULL);
+}
+
+
