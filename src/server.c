3325533d20e08cbfbe01ef3e57937c7d91039d82
Игорь <smartello@smartello-desktop.(none)>
2009-12-18 04:39:13 +0300

    server.c 2

--- src/server.c
+++ src/server.c
@@ -19,3 +19,63 @@ Description: server.c
 #include "server.h"
 
 
+void server_init(void)
+{
+    server.fStop = 0;
+    STAILQ_INIT( &server.sclients.cliqueue );
+    server.sclients.maxfd = -1;
+    mqlog( "Server initialized");
+}
+
+void server_start(int port)
+{
+    int servfd = -1;
+    int fAdded = 0;
+    struct client *cliiter = NULL;
+
+    mqlog( "Wanna start serving at port %d %d", port, server.fStop);
+    if ((servfd = pop3_bindsock(port)) < 0) {
+        server.fStop = 1;
+        mqlog( "Error while setup socket. Escape from server routing." );
+        return;
+    }
+
+    client_add(servfd);
+    while (1) {
+        FD_ZERO(&server.sclients.read_set);
+        server.sclients.maxfd = -1;
+        STAILQ_FOREACH(cliiter, &server.sclients.cliqueue, next) {
+            if (server.sclients.maxfd < cliiter->fd) server.sclients.maxfd = cliiter->fd;
+            FD_SET( cliiter->fd, &server.sclients.read_set);
+        }
+        server.sclients.maxfd++;
+
+        mqlog( "Selecting %d...", server.sclients.maxfd );
+        /*will be watched to see if characters  become  available  for
+       reading  (more  precisely, to see if a read will not block; in particu-
+       lar, a file descriptor is also ready on end-of-file)*/
+
+       if (pselect(server.sclients.maxfd, &server.sclients.read_set, NULL, NULL, NULL, NULL) == -1) {
+            mqlog( "Error in select(), but maybe signal arrived. server.fStop = %d", server.fStop );
+            if (server.fStop == 1) break;
+            continue;  //continue while
+        }
+
+        mqlog( "Selecting done..." );
+        STAILQ_FOREACH(cliiter, &server.sclients.cliqueue, next) {
+            if (FD_ISSET(cliiter->fd, &server.sclients.read_set)) {
+                if (cliiter->fd == servfd) {
+                    handle_client(client_add(client_accept(servfd)), 1);
+                    break;
+                } else {
+                    mqlog( "Need client_handle_request for %d", cliiter->fd );
+                    if (handle_client(cliiter->fd, 0) <= 0) {
+                        client_remove( cliiter );
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+
